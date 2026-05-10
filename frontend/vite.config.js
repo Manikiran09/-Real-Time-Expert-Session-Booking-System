dcf24fad81bf3fa2537b95b9a2e278fab3d76f8a
Gandu Manikaran <manikiran649@gmail.com>
2026-05-10 08:42:57 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,13 +9,13 @@ export default defineConfig({
     proxy: {
       // Proxy API calls to the backend server running on port 5000
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://real-time-expert-session-booking-system-mlsz.onrender.com',
         changeOrigin: true,
         secure: false,
       },
       // Proxy socket.io websocket connections
       '/socket.io': {
-        target: 'http://localhost:5000',
+        target: 'https://real-time-expert-session-booking-system-mlsz.onrender.com',
         ws: true,
         changeOrigin: true,
         secure: false,
